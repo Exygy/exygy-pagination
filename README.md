6c586a397749bd74802340c916dad91921ff133b
Emily Jablonski <emijablonski@gmail.com>
2025-04-08 13:47:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Exygy Pagination Assessment
+
+## Pagination
+
+This repo does not reflect any actual Exygy feature development. It was created only for the purpose of this assessment, and made intentionally buggy.
+
+This repo implements a directory of housing listings. There was a request for a new pagination and filtering feature that has already been implemented by a developer.
+
+The functional AC for this feature is as follows:
+1.  When the application loads, the user should be on page 1, and the default number of items per page should be 2.
+2.  A user should be able to change the number of items per page. The update takes place when a user clicks the "Update" button.
+3. A user should be able to navigate through the pages by clicking either the arrows or the numbers themselves.
+4. When a user updates the number of items per page, the selected page should reset to page 1.
+5. Only numbers greater than 0 are valid in the input for number of items per page.
+6. The number of pages should be dynamic based on the length of the list and the number of items per page.
+7. A user should be able to filter listings by selecting a unit type from a dropdown menu that should submit on selection.
+8. The unit type options should only reflect data in at least one listing, i.e. there should not be a "10 bedroom" option, if no listings have 10 bedrooms - so that no filter options return zero results.
+
+## Available Scripts
+
+This project was initialized with [Vite](https://vitejs.dev/) and Typescript.
+
+In the project directory, you can run:
+
+### `yarn dev`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
---
+++ src/Listings.tsx
@@ -0,0 +1,144 @@
+import { SetStateAction, useEffect, useState } from "react"
+import mockData from "./mock-data.json"
+import { Address, InfoBlock } from "./components/InfoBlock"
+import { Button } from "./components/Button"
+import { ImageBlock } from "./components/ImageBlock"
+import { Input } from "./components/Input"
+import { Pagination } from "./components/Pagination"
+import { RentUnit } from "./enums"
+import "./Listings.scss"
+
+export interface UnitTable {
+  minIncomeMaximum: number
+  minIncomeMinimum: number
+  rentMaximum: number
+  rentMinimum: number
+  rentUnit: RentUnit
+  type: "studio" | "oneBdrm" | "twoBdrm" | "threeBdrm" | "fourBdrm"
+}
+
+export interface Listing {
+  address: Address
+  deadline: string
+  id: string
+  imageLabels: string[]
+  imageURL: string
+  listingLabels: string[]
+  name: string
+  tableHeader: string | null
+  tableSubheader: string | null
+  unitTableData: UnitTable[]
+}
+
+const allData: Listing[] = mockData.sort((a, b) =>
+  a.name < b.name ? -1 : a.name > b.name ? 1 : 0
+) as Listing[]
+
+export const Listings = () => {
+  const [error, setError] = useState<string | null>(null)
+  const [itemsPerPage, setItemsPerPage] = useState(3)
+  const [itemsPerPageInput, setItemsPerPageInput] = useState(3)
+  const [page, setPage] = useState(1)
+  const [listingData, setListingData] = useState<Listing[]>(allData)
+  const [items, setItems] = useState<Listing[]>([])
+  const [unitTypeFilter, setUnitTypeFilter] = useState("")
+
+  const numSteps = Math.ceil(listingData.length / itemsPerPage)
+
+  const unitTypeOptions = [
+    { value: "studio", label: "Studio" },
+    { value: "oneBdrm", label: "1 BR" },
+    { value: "twoBdrm", label: "2 BR" },
+    { value: "threeBdrm", label: "3 BR" },
+    { value: "fourBdrm", label: "4 BR" },
+  ]
+
+  function onUnitTypeChange(e: { target: { value: SetStateAction<string> } }) {
+    setUnitTypeFilter(e.target.value)
+  }
+
+  useEffect(() => {
+    if (!unitTypeFilter) {
+      setListingData(allData)
+    } else {
+      setListingData(
+        allData.filter((listing) =>
+          listing.unitTableData.some((unit) => unit.type === unitTypeFilter)
+        )
+      )
+    }
+  }, [listingData, unitTypeFilter])
+
+  useEffect(() => {
+    setItems(
+      listingData.slice((page - 1) * itemsPerPage, page * itemsPerPage + 1)
+    )
+  }, [itemsPerPage, listingData, page])
+
+  return (
+    <div className={"content"}>
+      <div className={"container"}>
+        <div className={"filter-container"}>
+          <div className={"filters"}>
+            <div className={"update-items"}>
+              <Input
+                label={"Items per page"}
+                setValue={setItemsPerPageInput}
+                value={itemsPerPageInput}
+                setErrorMessage={setError}
+              />
+              <Button
+                onClick={() => {
+                  if (!itemsPerPageInput || itemsPerPageInput < 1) {
+                    setError("Must be greater than 0")
+                  } else {
+                    setItemsPerPage(itemsPerPageInput)
+                  }
+                }}
+              >
+                Update
+              </Button>
+            </div>
+            {error && <div className={"error-message"}>{error}</div>}
+            <div className="update-items">
+              <select onChange={onUnitTypeChange} value={unitTypeFilter}>
+                <option value="">Unit Type</option>
+                {unitTypeOptions.map((option, index) => (
+                  <option value={option.value} key={index}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <Pagination
+            numSteps={numSteps}
+            selected={page}
+            setSelected={setPage}
+          />
+        </div>
+        <div className={"listings"}>
+          {items.map((listing, index) => {
+            return (
+              <div className="listing" key={index}>
+                <ImageBlock
+                  imageURL={listing.imageURL}
+                  deadline={listing.deadline}
+                  labels={listing.imageLabels}
+                />
+                <InfoBlock
+                  title={listing.name}
+                  address={listing.address}
+                  tableHeader={listing.tableHeader}
+                  tableSubheader={listing.tableSubheader}
+                  labels={listing.listingLabels}
+                  unitRows={listing.unitTableData}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
